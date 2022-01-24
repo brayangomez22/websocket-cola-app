9a0acc34cb2f549864b54b3f7929871be5fb4683
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2022-01-24 15:27:14 -0500

    Added the design

--- sockets/sockets.controller.js
+++ sockets/sockets.controller.js
@@ -1,10 +1,4 @@
 const socketController = (socket) => {
-	console.log('connected client', socket.id);
-
-	socket.on('disconnect', () => {
-		console.log('disconnected client', socket.id);
-	});
-
 	socket.on('send-message', (payload, callback) => {
 		const id = 123456;
 		callback(id);
